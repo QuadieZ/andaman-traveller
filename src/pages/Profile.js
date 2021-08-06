2851c3b4e0b3f5e15eec95282ca0b310093e7d17
quadiez <dduidokdoi@gmail.com>
2021-08-06 15:06:33 +0700

    feat: profile card

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,11 +2,29 @@ import { Avatar, Heading, HStack, Stack, Text } from "@chakra-ui/react";
 
 const ProfileCard = ({ name, level }) => {
   return (
-    <Stack>
-      <Avatar src="https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094623-stock-illustration-female-avatar-woman.jpg" />
-      <Stack bgColor="white" border="2px" borderColor="gray.400">
-        <Heading>{name}</Heading>
-        <Text>นักเดินทางระดับ{level}</Text>
+    <Stack spacing="-2.2rem" align="center">
+      <Avatar
+        boxSize="4.8rem"
+        border="1px"
+        src="https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094623-stock-illustration-female-avatar-woman.jpg"
+      />
+      <Stack
+        bgColor="white"
+        border="2px"
+        borderColor="gray.400"
+        borderRadius="20px"
+        pt="3rem"
+        pb="1.5rem"
+        w="100%"
+        align="center"
+        spacing="0.2rem"
+      >
+        <Heading fontSize="1.7rem" fontWeight="medium" color="gray.600">
+          {name}
+        </Heading>
+        <Text color="gray.500" fontWeight="light">
+          นักเดินทางระดับ{level}
+        </Text>
       </Stack>
     </Stack>
   );
@@ -15,9 +33,9 @@ const ProfileCard = ({ name, level }) => {
 const Profile = () => {
   console.log("hi");
   return (
-    <>
+    <Stack px="5vw" py="5vh">
       <ProfileCard name="จอห์น ลี" level="เริ่มต้น" />
-    </>
+    </Stack>
   );
 };
 
