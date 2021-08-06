990ddcfb4161c4991a782a88afd0f7895f9afe4e
quadiez <dduidokdoi@gmail.com>
2021-08-06 11:59:57 +0700

    feat: gradient bg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Box } from "@chakra-ui/react";
 import { Route, Switch } from "react-router-dom";
 
 import { Home, Places } from "./pages";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/places" component={Places} />
-    </Switch>
+    <Box w="100%" h="100vh" bgGradient="linear(#C0E1FF 30%, #E79BDF )">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/places" component={Places} />
+      </Switch>
+    </Box>
   );
 }
 
