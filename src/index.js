5471673dcb22da86443465501eff3eae9c507144
quadiez <dduidokdoi@gmail.com>
2021-08-06 14:47:29 +0700

    feat: setup font

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/kanit";
+import theme from "./theme";
 
 ReactDOM.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
