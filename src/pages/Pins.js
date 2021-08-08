c90b852065f664fe33226079899ad30d26c0b2cf
quadiez <dduidokdoi@gmail.com>
2021-08-08 22:47:51 +0700

    feat: page pin placeholder

---
+++ src/pages/Pins.js
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+import { Navigation } from "../components/Navigation";
+
+const Pins = () => {
+  return (
+    <>
+      <Text>This is pins page</Text>
+      <Navigation />
+    </>
+  );
+};
+
+export default Pins;
